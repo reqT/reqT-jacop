39282c919b286f7446afc6d8498f5f305d9a1682
bjornregnell <bjorn.regnell@cs.lth.se>
2024-02-14 19:44:34 +0100

    add scala-cli project + package script

---
+++ package.sh
@@ -0,0 +1,2 @@
+VER="1.0.0"
+scala-cli package . --library -o ".scala-build/reqt-jacop_3-$VER.jar"
---
+++ project.scala
@@ -0,0 +1,4 @@
+//> using scala 3.3.1
+//> using dep "org.jacop:jacop:4.10.0"
+//> using dep "reqt-constraints:reqt-constraints:1.0.0,url=https://github.com/reqT/reqT-constraints/releases/download/v1.0.0/reqt-constraints_3-1.0.0.jar"
+//> using test.dep "org.scalameta::munit:0.7.29"
